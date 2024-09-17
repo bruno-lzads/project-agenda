5aa1e49d980ab296eec4406081c92fdb3a9a4671
Bruno Luiz <bruno.luiz.3110@gmail.com>
2024-09-17 19:49:31 -0300

    Criando tabelas e formulários

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'email', 'created_date', 'category',
+    ordering = '-id', #Ordena a lista
+    search_fields = 'id', 'first_name', 'email',
+    list_per_page = 10
+    list_max_show_all = 15 #Lime de campos ao mostrar tudo
+    #list_editable = 'first_name', 'last_name' #edita o input
+    list_display_links = 'id', 'first_name'
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id', #Ordena a lista
