32b86602a667739230a1c1f3a4e667f7ce39b8b5
Bruno Luiz <bruno.luiz.3110@gmail.com>
2024-10-29 22:30:43 -0300

    Create DELETE

--- contact/forms.py
+++ contact/forms.py
@@ -1,32 +1,58 @@
+# https://docs.djangoproject.com/en/5.1/ref/forms/widgets/
+
 from django.core.exceptions import ValidationError
 from django import forms
 
 from . import models
 
 class ContactForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields['first_name'].widget.attrs.update({
+            'placeholder': 'Your name...'
+        })
+        
+        self.fields['last_name'].widget.attrs.update({
+            'placeholder': 'Your last name...'
+        })
+    
     class Meta:
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone',
+            'email', 'description', 'category',
         )
+        # widgets = {
+        #     'first_name': forms.PasswordInput()
+        # } #muda os widgets dos input
+        
     
     def clean(self):
         cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
         
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mesagem de erro',
+        if last_name == first_name:
+            
+            msg = ValidationError(
+                'Last name cannot be the same as first name',
                 code='invalid'
             )
-        )
-        
-        self.add_error(
-            'last_name',
-            ValidationError(
-                'Mesagem de erro',
-                code='invalid'
-            )
-        )
+            
+            self.add_error('last_name', msg)
         
         return super().clean()
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Mensagem de erro',
+                    code='invalid'
+                )
+            )
+        return first_name
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,5 +1,6 @@
 from typing import Any, Dict
 from django.shortcuts import render
+from django.urls import reverse
 
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, render, redirect
@@ -9,16 +10,19 @@ from contact.forms import ContactForm
 from contact.models import Contact
 
 
-
-from contact.models import Contact
-
-
-
 def create(request):
+    form_action = reverse('contact:create')
     if request.method == 'POST':
+        form = ContactForm(request.POST)
+        
         context = {
-        'form': ContactForm(request.POST)
+            'form': form,
+            'form_action': form_action,
         }
+        
+        if form.is_valid():
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.pk)
 
         return render(
             request,
@@ -27,11 +31,69 @@ def create(request):
         )
     
     context = {
-        'form': ContactForm()
+        'form': ContactForm(),
+        'form_action': form_action,
     }
 
     return render(
         request,
         'contact/create.html',
         context
+    )
+    
+def update(request, contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    form_action = reverse('contact:update', args=(contact_id,))
+    
+    if request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+        
+        context = {
+            'form': form,
+            'form_action': form_action,
+        }
+        
+        if form.is_valid():
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.pk)
+
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
+    
+    context = {
+        'form': ContactForm(instance=contact),
+        
+        'form_action': form_action,
+    }
+
+    return render(
+        request,
+        'contact/create.html',
+        context
+    )
+    
+def delete(request, contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    
+    confirmation = request.POST.get('confirmation', 'no')
+    print('confirmation', confirmation)
+    
+    if confirmation == 'yes':    
+        contact.delete()
+        return redirect('contact:index')
+    
+    return render(
+        request,
+        'contact/contact.html',
+        {
+            'contact': contact,
+            'confirmation': confirmation,
+        }
     )
