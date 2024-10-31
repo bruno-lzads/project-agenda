e53360d4c1aa49263d82bbb72413da8033c66531
Bruno Luiz <bruno.luiz.3110@gmail.com>
2024-10-30 22:03:35 -0300

    Creating our contact archive

--- contact/forms.py
+++ contact/forms.py
@@ -6,6 +6,13 @@ from django import forms
 from . import models
 
 class ContactForm(forms.ModelForm):
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept': 'image/*',
+            }
+        )
+    )
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
@@ -22,6 +29,7 @@ class ContactForm(forms.ModelForm):
         fields = (
             'first_name', 'last_name', 'phone',
             'email', 'description', 'category',
+            'picture',
         )
         # widgets = {
         #     'first_name': forms.PasswordInput()
